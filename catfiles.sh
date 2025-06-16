37d7354f1c7670290faad1cb46cb5520c4de1c4a
Dana Gretton <dana.gretton@gmail.com>
2025-06-16 15:36:45 -0400

    catfiles . not ../giantt

--- catfiles.sh
+++ catfiles.sh
@@ -1,4 +1,4 @@
-./dirtree.sh ../giantt/
+./dirtree.sh .
 python ./file_concatenator.py . concatenated-giantt-source.txt
 cp actual-current-directory-tree.txt ~/Downloads/
 cp concatenated-giantt-source.txt ~/Downloads/
